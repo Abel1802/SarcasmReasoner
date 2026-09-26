1e0bd67caebe68e17f3d759f316dc58388bdc805
yuqing0304 <yuqingelsa@gmail.com>
2026-09-26 15:10:57 +0200

    grpo_rm training on mustard

---
+++ scripts/slurm/mustard_grpo_rm_best_of_8_1gpu.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#SBATCH --job-name=en_best_of_8_grpo_rm
+#SBATCH --partition=gpu_h100
+
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=16
+#SBATCH --gpus-per-node=1
+
+#SBATCH --mem=180G
+#SBATCH --time=12:00:00
+
+#SBATCH --output=logs/mustard_best_of_8_grpo_rm_%j.out
+#SBATCH --error=logs/mustard_best_of_8_grpo_rm_%j.err
+
+#SBATCH --no-requeue
+
+set -e
+
+source activate ms2
+
+bash configs/mustard/grpo_rm/best_of_8_1gpu.sh
